c6eca2f4bf8cb80fb033c8beb7a0d401d641079a
John Navitsky <johnn@cray.com>
2022-04-26 15:54:37 -0700

    SCICD-245 create function to create a dvs reload config
    IF there is a playbook called ncn_upgrade.yml in the COS working branch AND there is a
    SHS in the ncn_personalization config, create a new config and return the config
    name.
    
    ncn-m001:/admin/johnn/install # ./cne-install -i install.yaml install -r debug --ncn-personalization ncn-personalization-w-shs
    INFO STAGE SUMMARY: begin_stage=debug, end_stage=debug
    INFO Beginning stage: debug
    INFO   Updating cos layer of ncn_dvs_reload with commit 67f096877db99ca692e20193351b3bec7127dcad
    INFO   DVS reload config saved in /admin/johnn/install/state/dvs_reload.json
    INFO   Using DVS reload config ncn_dvs_reload
    INFO   stage completed in 0:00:04
    INFO Install completed in 0:00:04
    ncn-m001:/admin/johnn/install # cray cfs configurations describe ncn_dvs_reload --format json
    {
      "lastUpdated": "2022-04-26T22:51:06Z",
      "layers": [
        {
          "cloneUrl": "https://api-gw-service-nmn.local/vcs/cray/slingshot-host-software-config-management.git",
          "commit": "4fe65254f29fb4c73a0715f901f75bf9952ce558",
          "name": "slingshot-host-software-1.7-integration",
          "playbook": "shs_mellanox_install.yml"
        },
        {
          "cloneUrl": "https://vcs.lemondrop.dev.cray.com/vcs/cray/cos-config-management.git",
          "commit": "67f096877db99ca692e20193351b3bec7127dcad",
          "name": "cos-2.2-integration",
          "playbook": "ncn-upgrade.yml"
        }
      ],
      "name": "ncn_dvs_reload"
    }
    ncn-m001:/admin/johnn/install #

--- utils/ShastaUpdate.py
+++ utils/ShastaUpdate.py
@@ -507,7 +507,11 @@ def update_cfs_commits(args, cfs_template_arg):
     cfs_template = copy.deepcopy(cfs_template_arg)
 
     del cfs_template["name"]
-    del cfs_template["lastUpdated"]
+    # don't die if not found
+    try:
+        del cfs_template["lastUpdated"]
+    except Exception:
+        pass
 
     def find_substr(substr):
         """Return the index of the element containing the substring.  This
@@ -900,6 +904,83 @@ def customize_cos_compute_image(args):
         install_logger.info("  Resulting customized image id {}".format(image_id))
     elif os.path.exists(bos_file):
         os.remove(bos_file)
+
+
+def create_dvs_reload_config(args):
+    """
+    build a ncn reload config, if possible
+    """
+
+    # we need the location_dict to lookup up information about the product
+    location_dict = utils.get_product_catalog(connection, load_prods(args))
+
+    # get the cos clone url
+    cos_clone_url = location_dict[get_prod_key(args, "cos")]["clone_url"]
+
+    # get the commit and branch
+    cos_commit, cos_branch = current_repo_branch(args, "cos-config-management", None)
+
+    # get a list of the files in the cos config
+    git = utils.git(args, connection)
+    repo = "cos-config-management"
+    cos = git.clone(repo)
+    git.checkout(repo, cos_branch)
+    git.pull(repo, quiet=True)
+    repofiles = os.listdir(cos)
+    git.cleanup(repo)
+
+    # ncn-upgrade.yml must be in the cos config for this method
+    if "ncn-upgrade.yml" in repofiles:
+
+        install_logger.debug("found ncn-upgrade.yml in cos config")
+
+        cos_layer = {'cloneUrl': cos_clone_url,
+            'commit': cos_commit,
+            'name': cos_branch,
+            'playbook': 'ncn-upgrade.yml'}
+
+        install_logger.debug("built cos layer of {}".format(cos_layer))
+
+        # get the SHS layer from the ncn_personalization config
+        template_name = args.get("ncn_personalization")
+        cfs_template = None
+        cfs_template = json.loads(connection.sudo("cray cfs configurations describe {} --format json".format(
+            template_name)).stdout)
+
+        shs_layer = None
+        for i in cfs_template["layers"]:
+            if 'slingshot-host-software-config-management' in i["cloneUrl"]:
+                shs_layer = i
+
+        if shs_layer:
+
+            install_logger.debug("found shs layer in {}".format(args.get("ncn_personalization")))
+            install_logger.debug("found shs layer of {}".format(shs_layer))
+
+            template_name = "ncn_dvs_reload"
+            base_file = "dvs_reload.json"
+
+            # build a current config
+            cfs_template = update_cfs_commits(args,
+                { "layers": [ shs_layer, cos_layer], "name": template_name })
+
+            file_location = os.path.join(get_dirs(args, "state"), base_file)
+            with open(file_location, 'w', encoding='UTF-8') as fhandle:
+                json.dump(cfs_template, fhandle)
+
+            connection.sudo("cray cfs configurations update {} --file {} --format json".format(
+                template_name, file_location))
+
+            install_logger.info("  DVS reload config saved in {}".format(file_location))
+            install_logger.info("  Using DVS reload config {}".format(template_name))
+
+            return template_name, file_location
+
+        else:
+            raise NCNPersonalization("there is no SHS layer")
+    else:
+        raise NCNPersonalization("there is no ")
+
 
 def create_cos_cfs_config(args):
     """
