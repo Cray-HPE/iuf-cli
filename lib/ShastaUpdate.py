cd34022e2c4c8da9df70cee309e1539347f71c1d
John Navitsky <johnn@cray.com>
2022-08-24 15:52:33 -0700

    make more clear which installer you're running mock out calls to product-installer for all stages

--- lib/ShastaUpdate.py
+++ lib/ShastaUpdate.py
@@ -49,6 +49,12 @@ def get_prods(config):
 
 def stub_pre_install_check(config):
     install_logger.info("  checked system ready for update and checked software compatability")
+    for prod in config.location_dict:
+        cmd = '../common/product-install -m {} install -r {}'.format(prod.work_dir, config.stages.current_stage)
+        logname = 'log/{}-{}'.format(prod.name, config.stages.current_stage)
+        install_logger.info('    Running {}'.format(cmd))
+        install_logger.info('    Logging to {}'.format(logname))
+        config.connection.sudo('{}'.format(cmd), timeout=1800, store_output=logname)
 
 def stub_deliver_product(config):
     install_logger.info("  product content uploaded to system")
@@ -62,12 +68,30 @@ def stub_deliver_product(config):
 
 def stub_update_config(config):
     install_logger.info("  updated VCS branches, run automated config updates")
+    for prod in config.location_dict:
+        cmd = '../common/product-install -m {} install -r {}'.format(prod.work_dir, config.stages.current_stage)
+        logname = 'log/{}-{}'.format(prod.name, config.stages.current_stage)
+        install_logger.info('    Running {}'.format(cmd))
+        install_logger.info('    Logging to {}'.format(logname))
+        config.connection.sudo('{}'.format(cmd), timeout=1800, store_output=logname)
 
 def stub_deploy_product(config):
     install_logger.info("  deployed services to system")
+    for prod in config.location_dict:
+        cmd = '../common/product-install -m {} install -r {}'.format(prod.work_dir, config.stages.current_stage)
+        logname = 'log/{}-{}'.format(prod.name, config.stages.current_stage)
+        install_logger.info('    Running {}'.format(cmd))
+        install_logger.info('    Logging to {}'.format(logname))
+        config.connection.sudo('{}'.format(cmd), timeout=1800, store_output=logname)
 
 def stub_post_install_check(config):
     install_logger.info("  verified product successfully deployed")
+    for prod in config.location_dict:
+        cmd = '../common/product-install -m {} install -r {}'.format(prod.work_dir, config.stages.current_stage)
+        logname = 'log/{}-{}'.format(prod.name, config.stages.current_stage)
+        install_logger.info('    Running {}'.format(cmd))
+        install_logger.info('    Logging to {}'.format(logname))
+        config.connection.sudo('{}'.format(cmd), timeout=1800, store_output=logname)
 
 def stub_build_images(config):
     install_logger.info("  build and configure images")
