ca99fea9255653b3cbc3b1c74ed63f2084896be3
John Navitsky <johnn@cray.com>
2022-08-25 08:47:24 -0700

    test

--- lib/ShastaUpdate.py
+++ lib/ShastaUpdate.py
@@ -52,18 +52,20 @@ def stub_pre_install_check(config):
     for prod in config.location_dict:
         cmd = '../common/product-install -m {} install -r {}'.format(prod.work_dir, config.stages.current_stage)
         logname = 'log/{}-{}'.format(prod.name, config.stages.current_stage)
-        install_logger.info('    Running {}'.format(cmd))
-        install_logger.info('    Logging to {}'.format(logname))
+        #install_logger.info('    Running {}'.format(cmd))
+        #install_logger.info('    Logging to {}'.format(logname))
+        print('vvvvvv')
         config.connection.sudo('{}'.format(cmd), timeout=1800, store_output=logname)
+        print('^^^^^^')
 
 def stub_deliver_product(config):
     install_logger.info("  product content uploaded to system")
     for prod in config.location_dict:
         cmd = '../common/product-install -m {} install -r {}'.format(prod.work_dir, config.stages.current_stage)
         logname = 'log/{}-{}'.format(prod.name, config.stages.current_stage)
-        install_logger.info('    Running {}'.format(cmd))
-        install_logger.info('    Logging to {}'.format(logname))
-        config.connection.sudo('{}'.format(cmd), timeout=1800, store_output=logname)
+        #install_logger.info('    Running {}'.format(cmd))
+        #install_logger.info('    Logging to {}'.format(logname))
+        config.connection.sudo('{}'.format(cmd), timeout=1800, tee=True, store_output=logname)
         #install_logger.info("../common/product-install -m {} install -r deliver_product".format(prod.work_dir))
 
 def stub_update_config(config):
@@ -71,27 +73,27 @@ def stub_update_config(config):
     for prod in config.location_dict:
         cmd = '../common/product-install -m {} install -r {}'.format(prod.work_dir, config.stages.current_stage)
         logname = 'log/{}-{}'.format(prod.name, config.stages.current_stage)
-        install_logger.info('    Running {}'.format(cmd))
-        install_logger.info('    Logging to {}'.format(logname))
-        config.connection.sudo('{}'.format(cmd), timeout=1800, store_output=logname)
+        #install_logger.info('    Running {}'.format(cmd))
+        #install_logger.info('    Logging to {}'.format(logname))
+        config.connection.sudo('{}'.format(cmd), timeout=1800, tee=True, store_output=logname)
 
 def stub_deploy_product(config):
     install_logger.info("  deployed services to system")
     for prod in config.location_dict:
         cmd = '../common/product-install -m {} install -r {}'.format(prod.work_dir, config.stages.current_stage)
         logname = 'log/{}-{}'.format(prod.name, config.stages.current_stage)
-        install_logger.info('    Running {}'.format(cmd))
-        install_logger.info('    Logging to {}'.format(logname))
-        config.connection.sudo('{}'.format(cmd), timeout=1800, store_output=logname)
+        #install_logger.info('    Running {}'.format(cmd))
+        #install_logger.info('    Logging to {}'.format(logname))
+        config.connection.sudo('{}'.format(cmd), timeout=1800, tee=True, store_output=logname)
 
 def stub_post_install_check(config):
     install_logger.info("  verified product successfully deployed")
     for prod in config.location_dict:
         cmd = '../common/product-install -m {} install -r {}'.format(prod.work_dir, config.stages.current_stage)
         logname = 'log/{}-{}'.format(prod.name, config.stages.current_stage)
-        install_logger.info('    Running {}'.format(cmd))
-        install_logger.info('    Logging to {}'.format(logname))
-        config.connection.sudo('{}'.format(cmd), timeout=1800, store_output=logname)
+        #install_logger.info('    Running {}'.format(cmd))
+        #install_logger.info('    Logging to {}'.format(logname))
+        config.connection.sudo('{}'.format(cmd), timeout=1800, tee=True, store_output=logname)
 
 def stub_build_images(config):
     install_logger.info("  build and configure images")
